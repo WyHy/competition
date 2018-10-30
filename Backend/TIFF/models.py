af21506d9253fcd617c31a29db1a637d306903fa
zero <grayautumn15@126.com>
2018-10-30 17:33:39 +0800

    添加 TIFF status 字段

--- Backend/TIFF/models.py
+++ Backend/TIFF/models.py
@@ -7,6 +7,12 @@ class Image(models.Model):
     """
     病理图像 原图信息
     """
+    STATUS_CHOICES = (
+        ("CREATED", "待处理"),
+        ("SUCCESS", "处理完成"),
+        ("FAIL", "处理失败"),
+        ("DELETED", "已删除"),
+    )
 
     id = models.AutoField(primary_key=True, verbose_name='唯一主键')
     name = models.CharField(max_length=64, verbose_name=u"图像名称")
@@ -15,6 +21,8 @@ class Image(models.Model):
     result_auto = models.CharField(max_length=32, verbose_name=u"算法诊断结果", default='-')
     result_manual = models.CharField(max_length=32, verbose_name=u"医生诊断结果", default='-')
     result_status = models.CharField(max_length=32, verbose_name=u"标准诊断结果", default='-')
+    status = models.CharField(choices=STATUS_CHOICES, default="CREATED", verbose_name=u"状态")
+    remark = models.TextField(verbose_name=u"故障原因", blank=True, null=True)
 
     create_time = models.DateTimeField(verbose_name=u"创建时间", auto_now_add=True)
     update_time = models.DateTimeField(verbose_name=u"处理时间", auto_now=True)
