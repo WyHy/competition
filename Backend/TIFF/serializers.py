f034db4dffd8b2e797fc8a8f5ab30ba62e2d9892
zero <grayautumn15@126.com>
2018-11-17 18:31:49 +0800

    update

--- Backend/TIFF/serializers.py
+++ Backend/TIFF/serializers.py
@@ -7,8 +7,8 @@ class ImageSerializer(serializers.ModelSerializer):
     class Meta:
         model = Image
         fields = (
-            'id', 'name', 'case_no', 'path', 'progress', 'result_auto', 'result_manual', 'result_status', 'is_valid',
-            'is_valid', 'create_time', 'update_time'
+            'id', 'name', 'case_no', 'path', 'progress', 'result_auto', 'result_manual', 'result_status', 'status',
+            'is_valid', 'is_valid', 'create_time', 'update_time'
         )
 
 
