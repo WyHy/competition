c6534c79bd04561d13b279c86bad2f1f00a0afaf
Zero <grayautumn15@126.com>
2018-11-18 20:14:32 +0800

    update

--- Backend/tools/slide_fields_value_control.py
+++ Backend/tools/slide_fields_value_control.py
@@ -19,7 +19,7 @@ def disable_slide_validation():
         }
 
         for image_id in ids:
-            response = requests.patch('http://%s/api/v1/images/%s' % (HOST, image_id), json=image, headers=HEADER)
+            response = requests.patch('http://%s/api/v1/images/%s/' % (HOST, image_id), json=image, headers=HEADER)
             if response.status_code == 200:
                 pass
             else:
@@ -41,7 +41,7 @@ def clean_slide_diagnose_result():
         }
 
         for image_id in ids:
-            response = requests.patch('http://%s/api/v1/images/%s' % (HOST, image_id), json=image, headers=HEADER)
+            response = requests.patch('http://%s/api/v1/images/%s/' % (HOST, image_id), json=image, headers=HEADER)
             if response.status_code == 200:
                 pass
             else:
@@ -54,13 +54,11 @@ def make_selected_valid(file_path):
 
         for slide_name in lines:
             image = None
-            for item in ['.kfb', '.tif']:
-                response = requests.get('http://%s/api/v1/images/?name=%s' % (HOST, slide_name + item), headers=HEADER)
-                if response.status_code == 200 and response.json():
-                    data = response.json()
-                    if data:
-                        image = data[0]['id']
-                        break
+            response = requests.get('http://%s/api/v1/images/?case_no=%s' % (HOST, slide_name), headers=HEADER)
+            if response.status_code == 200 and response.json():
+                data = response.json()
+                if data:
+                    image = data[0]['id']
             else:
                 raise Exception("NO TIFF NAMED %s" % slide_name)
 
@@ -68,17 +66,18 @@ def make_selected_valid(file_path):
                 "is_valid": "YES",
             }
 
-            response = requests.patch('http://%s/api/v1/images/%s' % (HOST, image), json=data, headers=HEADER)
+            response = requests.patch('http://%s/api/v1/images/%s/' % (HOST, image), json=data, headers=HEADER)
             if response.status_code == 200:
                 pass
             else:
                 print(response.json())
+                break
 
 
 if __name__ == '__main__':
-    disable_slide_validation()
-    clean_slide_diagnose_result()
+    # disable_slide_validation()
+    # clean_slide_diagnose_result()
 
-    file_path = ""
+    file_path = "./valid_slide_name.txt"
     make_selected_valid(file_path)
 
